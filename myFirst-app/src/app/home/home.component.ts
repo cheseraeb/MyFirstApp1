de73161c7b713fca3fc2ed9fd4ab7ed77fbe2b1d
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-06-12 16:37:31 +0200

    update 1.5

--- myFirst-app/src/app/home/home.component.ts
+++ myFirst-app/src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle = 'Welcome to Angular 2';
+  myValue = "I Love Cheeze";
+  trueOrFalse = true;
   constructor() { }
 
   ngOnInit() {
