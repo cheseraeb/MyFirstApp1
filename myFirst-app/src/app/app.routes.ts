bac404bcabf93d40563c35f8fefa2dc323c79e25
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-06-15 07:49:32 +0200

    all

--- myFirst-app/src/app/app.routes.ts
+++ myFirst-app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { DirectoryComponent } from './directory/directory.component';
 
 export const appRoutes: Routes = [
+  { path: 'directory/:cheeze1', component: DirectoryComponent },
   { path: 'directory', component: DirectoryComponent },
   { path: '',      component: HomeComponent },
   ];
--- myFirst-app/src/app/directory/directory.component.ts
+++ myFirst-app/src/app/directory/directory.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.css']
 })
 export class DirectoryComponent implements OnInit {
-
-  constructor() { }
+  cheeze1: string;
+  constructor(private route: ActivatedRoute) {
+      this.cheeze1 = route.snapshot.params['cheeze1'];
+   }
 
   ngOnInit() {
   }
